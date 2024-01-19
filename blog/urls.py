dbf1e7daaac15af89b4bfc2ada274bf938a08769
zz <951054670@qq.com>
2024-01-19 23:41:52 +0800

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('',views.post_list,name='post_list'),
-=======
     path('', views.post_list,name='post_list'),
->>>>>>> 54f00dccdf19a7562e151a18c22fc586011e07d3
 ]
