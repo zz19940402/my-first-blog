9644447cb2e62c141ada7b15cb1761d55fe566c5
Mr.zhang <122374994+zz19940402@users.noreply.github.com>
2024-01-19 23:22:22 +0800

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
 <<<<<<< HEAD
     path('',views.post_list,name='post_list'),
-=======
-    path('', views.post_list,name='post_list'),
->>>>>>> 54f00dccdf19a7562e151a18c22fc586011e07d3
 ]
