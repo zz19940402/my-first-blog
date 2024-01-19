dd6739ac29b7be4c531f9fbbdbf7228bdcc5b99c
Mr.zhang <122374994+zz19940402@users.noreply.github.com>
2024-01-19 23:23:05 +0800

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.创建一个视图
 def post_list(request):
-<<<<<<< HEAD
     return render(request, 'blog/post_list.html',{})
-=======
-    return render(request,'blog/post_list.html',{})
->>>>>>> 54f00dccdf19a7562e151a18c22fc586011e07d3
+
