38fc14a5c1dd73720abd4c7a424a8ea9425c11cb
zz <951054670@qq.com>
2024-01-22 22:02:16 +0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list,name='post_list'),
+    # 跳转页面
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # 显示图标
+    path('post/new/', views.post_new, name='post_new'), 
+    # 添加笔写入管理页面
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+# 跳转到新增的页面
+from django.shortcuts import redirect
+
 
 # 添加我们的视图
 def post_list(request):
@@ -12,3 +16,38 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+# 添加符号的视图
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+# 转到新增的视图
+def post_new(request):
+    if request.method == 'POST':
+        form= PostForm(request.POST)
+        # 判断是否有效，并保存
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form}) 
+
+# 缺少视图
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
