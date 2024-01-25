667c767026d59a2712967692b712983a2b08d154
zz <951054670@qq.com>
2024-01-25 13:39:26 +0800

    settings改变原来在提交给pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls',
-        'USER': 'postgres',
-        'PASSWORD': '123456.',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
